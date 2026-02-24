6963ee8b95fa99f704fb99cc5c84e797fd379ae7
ahmad-dev269 <muhammad.ahmad.p171@gmail.com>
2026-02-24 12:40:37 +0500

    resolve git problem ..

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+export default {
+  root: "test",   // root = project root
+  server: {
+    open: "/test/index.html",
+    host: '0.0.0.0', // Allow access from network (for mobile debugging)
+    port: 5173
+  }
+};
